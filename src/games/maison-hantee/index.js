2df2d9107cf6903068d062cfb1e46afc279966aa
achtaitaipai <charles.cailleteau@gmail.com>
2024-10-20 12:39:31 +0200

    update odyc

--- src/games/maison-hantee/index.js
+++ src/games/maison-hantee/index.js
@@ -1,4 +1,4 @@
-import { createGame, createSound } from "odyc"
+import { createGame, createSound } from "odyc";
 const sketuve = `
 		...4.44..
 		..44444..
@@ -9,7 +9,7 @@ const sketuve = `
 		.44444...
 		.56...65.
 		.5.....5.
-	`
+	`;
 const chat = `
 		00.......
 		0.....0.0
@@ -20,7 +20,7 @@ const chat = `
 		0000000..
 		0.....0..
 		0.....0..
-	`
+	`;
 const grimoire = `
        
 		.........
@@ -33,7 +33,7 @@ const grimoire = `
 		111101111
 		.........
 	
-    `
+    `;
 
 const fin = `
 		.0.....0.
@@ -45,8 +45,7 @@ const fin = `
 		..01010..
 		.0.0.0.0.
 		0.......0
-	`
-
+	`;
 
 const game = createGame({
   title: `LA MAISON HANTEE
@@ -74,12 +73,10 @@ const game = createGame({
     position: [4, 2],
   },
 
-
   templates: {
     //Fantome solide
     f: {
-      sprite:
-        `
+      sprite: `
 		...11...
 		..1111..
 		..0101..
@@ -89,8 +86,8 @@ const game = createGame({
 		.111111.
 		11111111
         11111111
-      ` ,
-    },//Fantome liquide
+      `,
+    }, //Fantome liquide
     q: {
       sprite: `
 		...11...
@@ -103,13 +100,12 @@ const game = createGame({
 		11111111
         11111111
       `,
-      solid: false
+      solid: false,
     },
 
     //Vampire haut
     v: {
-      sprite:
-        `
+      sprite: `
 		..11111..
 		001010100
 		.0111110.
@@ -119,12 +115,11 @@ const game = createGame({
 		.0000000.
 		.0000000.
 		.0000000.
-      `
+      `,
     },
     //Clown
     c: {
-      sprite:
-        `
+      sprite: `
 		..11111..
 		661010166
 		661141166
@@ -134,12 +129,11 @@ const game = createGame({
 		333454333
 		333434333
 		333454333
-	`
+	`,
     },
     //Couteau
     k: {
-      sprite:
-        `
+      sprite: `
 		.......14
 		......114
 		.....111.
@@ -154,8 +148,7 @@ const game = createGame({
     },
     //Vampire bas
     V: {
-      sprite:
-        `
+      sprite: `
 		.1000001
 		..00000.
 		..00000.
@@ -164,12 +157,13 @@ const game = createGame({
 		..00000.
 		..00000.
 		..00000.
-	`, dialog: "Oooh, quel petit chat mignon! J'ai déjà un animal domestique, c'est dommage!"
+	`,
+      dialog:
+        "Oooh, quel petit chat mignon! J'ai déjà un animal domestique, c'est dommage!",
     },
     //Balais
     b: {
-      sprite:
-        `
+      sprite: `
 		.......6
 		......6.
 		.....6..
@@ -179,12 +173,11 @@ const game = createGame({
 		5555....
 		555.....
 	`,
-      dialog: "Je suis le balais le plus rapide du monde."
+      dialog: "Je suis le balais le plus rapide du monde.",
     },
     //Sorciere
     w: {
-      sprite:
-        `
+      sprite: `
 		....00...
 		..000000.
 		...6116..
@@ -196,16 +189,15 @@ const game = createGame({
 		..000000.
 	`,
       dialog: "Abracadabra un chat tu seras!",
-      sound: createSound('POWERUP', 1988),
+      sound: createSound("POWERUP", 1988),
       onCollide: function (target) {
-        game.player.sprite = chat
-        target.remove()
-      }
+        game.player.sprite = chat;
+        target.remove();
+      },
     },
     //Grimoire
     g: {
-      sprite:
-        `
+      sprite: `
 		.44444445
 		.44444445
 		.44444445
@@ -217,13 +209,17 @@ const game = createGame({
 		.44444445
 	`,
       onCollide: async function (target) {
-        target.sprite = grimoire
-        await game.openDialog("Oh, un grimoire! Je vais essayer de retrouver une forme humaine! Arbadacarba, je ne veux plus être un chat!")
-        game.playSound('POWERUP', 666)
-        game.player.sprite = fin
-        await game.openDialog("Oups, ce n'est pas ce que j'avais prévu, je vais finalement rester ici.")
-        game.end('Fin...')
-      }
+        target.sprite = grimoire;
+        await game.openDialog(
+          "Oh, un grimoire! Je vais essayer de retrouver une forme humaine! Arbadacarba, je ne veux plus être un chat!",
+        );
+        game.playSound("POWERUP", 666);
+        game.player.sprite = fin;
+        await game.openDialog(
+          "Oups, ce n'est pas ce que j'avais prévu, je vais finalement rester ici.",
+        );
+        game.end("Fin...");
+      },
     },
 
     //Tete de mort
@@ -239,9 +235,9 @@ const game = createGame({
 		..10101..
 		..10101..
 	`,
-      dialog: "Je n'ai jamais pu sortir d'ici, je suis tombé sur un os..."
+      dialog: "Je n'ai jamais pu sortir d'ici, je suis tombé sur un os...",
     },
-    //Os 
+    //Os
     o: {
       sprite: `
 		......11.
@@ -255,15 +251,14 @@ const game = createGame({
 		.11......
 	`,
       onCollide: function (target) {
-        target.remove()
-        game.setAll('s', {
+        target.remove();
+        game.setAll("s", {
           solid: false,
-          dialog: 'Ohh merci pour ce bel os. Tu peux passer!'
-        })
+          dialog: "Ohh merci pour ce bel os. Tu peux passer!",
+        });
       },
       dialog: "Oh, un bel os, je le garde!",
-      sound: createSound('PICKUP', 9)
-
+      sound: createSound("PICKUP", 9),
     },
     //décor
     d: {
@@ -280,9 +275,9 @@ const game = createGame({
 	`,
       solid: false,
       onEnter: function (target) {
-        target.remove()
+        target.remove();
       },
-      sound: createSound('HIT', 9)
+      sound: createSound("HIT", 9),
     },
     //piege
     p: {
@@ -297,13 +292,15 @@ const game = createGame({
 		2.2.2.4.2
 		2.2.2.2.2
 		222222222
-	`},
+	`,
+    },
     //piege2
     P: {
       sprite: `
        222222222
 		.2.2.2.2.
-		.4.2.4.2.`},
+		.4.2.4.2.`,
+    },
     //mur
     x: {
       sprite: `
@@ -316,7 +313,7 @@ const game = createGame({
 		000000000
 		111111101
 		222222201
-	`
+	`,
     },
     //clef
     F: {
@@ -332,8 +329,10 @@ const game = createGame({
 		..55.....
 	`,
       dialog: "Chic! Une clé en or, je vais surement pouvoir la vendre chère!",
-      sound: createSound('PICKUP', 5),
-      onCollide: function (target) { target.remove() },
+      sound: createSound("PICKUP", 5),
+      onCollide: function (target) {
+        target.remove();
+      },
     },
     //Feu
     a: {
@@ -350,16 +349,16 @@ const game = createGame({
       solid: false,
       visible: false,
       onLeave: function (target) {
-        target.visible = true
+        target.visible = true;
       },
       onEnter: async function (target) {
         if (target.visible) {
-          game.playSound('EXPLOSION', 69)
-          await game.openDialog('Aïe!')
-          game.player.position = [40, 4]
-          game.setAll('a', { visible: false, solid: false })
+          game.playSound("EXPLOSION", 69);
+          await game.openDialog("Aïe!");
+          game.player.position = [40, 4];
+          game.setAll("a", { visible: false, solid: false });
         }
-      }
+      },
     },
     //trone
     i: {
@@ -376,11 +375,11 @@ const game = createGame({
 	`,
       onCollide: async function (target) {
         if (target.sprite !== sketuve) {
-          await game.playSound('EXPLOSION', 33)
+          await game.playSound("EXPLOSION", 33);
           target.sprite = sketuve;
         }
       },
-      dialog: "J'ai une envie subite de me faire les griffes."
+      dialog: "J'ai une envie subite de me faire les griffes.",
     },
     O: {
       sprite: `
@@ -394,7 +393,7 @@ const game = createGame({
 		....5....
 		..55555..
 	`,
-      dialog: 'Meilleur suceur\nDécerné à Droculo en 1756'
+      dialog: "Meilleur suceur\nDécerné à Droculo en 1756",
     },
     // chaudron
     u: {
@@ -409,7 +408,7 @@ const game = createGame({
 		.0000000.
 		..00000..
 	`,
-      dialog: "Hum, cette soupe a une drôle d'odeur..."
+      dialog: "Hum, cette soupe a une drôle d'odeur...",
     },
     //chien
     s: {
@@ -424,7 +423,8 @@ const game = createGame({
 		...2..2..
 		..22.22..
 	`,
-      dialog: "Comment faire pour que ce molosse me laisse passer? Si seulement j'avais un os...",
+      dialog:
+        "Comment faire pour que ce molosse me laisse passer? Si seulement j'avais un os...",
     },
     //chauve souris
     S: {
@@ -439,7 +439,7 @@ const game = createGame({
 		...000...
 		....0....
 	`,
-      dialog: "Certains fantômes sont gentils et te laisseront passer..."
+      dialog: "Certains fantômes sont gentils et te laisseront passer...",
     },
     j: { sprite: 7 },
   },
@@ -453,5 +453,5 @@ const game = createGame({
     xxcddcxx  xxx..xxx x....... ..Sxx..x xfqfffff xaaaxxax x......x
     x.......  .......x x.o..... ...xx.s. .qqfffff xxxaaaax x......x
     x.......  ...k...x xxxxxxxx xxxxxxxx xxxxxxxx xxxxxaxx xxxxxxxx
-  `
-})
+  `,
+});
